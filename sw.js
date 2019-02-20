365246a04569a38851ac4ddf39ba60b83f88eb08
Santiago Guerra <guerrasantiago19@gmail.com>
2019-02-20 03:03:45 -0500

    h

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-53e77cf4beb91e6ef27a.js"
+    "url": "webpack-runtime-988e1a1d4af56bbb708f.js"
   },
   {
     "url": "app-d6f90a9ad9974d38afce.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "27b7512959510be76fc25b96131fba70"
+    "revision": "fc02ee17fdacca99278118e290c2b486"
   },
   {
     "url": "0.e14ae67d7912639a18a7.css"
